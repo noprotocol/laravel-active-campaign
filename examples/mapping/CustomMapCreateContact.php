cce6da930debf53a7d7025cdf22b1e5f8b022507
Job Verplanke <job.verplanke@gmail.com>
2022-04-11 19:49:14 +0200

    Updated examples

--- examples/mapping/CustomMapCreateContact.php
+++ examples/mapping/CustomMapCreateContact.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace JobVerplanke\LaravelActiveCampaign\Examples;
 
+use Illuminate\Support\Facades\Route;
 use JobVerplanke\LaravelActiveCampaign\Contracts\Mapper;
 use Illuminate\Support\Enumerable;
+use JobVerplanke\LaravelActiveCampaign\Resources\Contact\CreateContact;
 
 class CustomMapCreateContact implements Mapper
 {
@@ -16,3 +18,18 @@ class CustomMapCreateContact implements Mapper
         ];
     }
 }
+
+Route::get('create-contact-custom-map', function () {
+    $data = collect(value: [
+        'email' => 'johndoe@example.com',
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+    ]);
+
+    $createContact = new CreateContact();
+    $response = $createContact
+        ->mapUsing(class: CustomMapCreateContact::class)
+        ->execute(data: $data);
+
+    return $response->json();
+});
--- examples/mapping/ExtendMapCreateContact.php
+++ examples/mapping/ExtendMapCreateContact.php
@@ -4,15 +4,33 @@ declare(strict_types=1);
 
 namespace JobVerplanke\LaravelActiveCampaign\Examples;
 
+use Illuminate\Support\Facades\Route;
 use JobVerplanke\LaravelActiveCampaign\Mapping\Contact\MapCreateContact;
 use Illuminate\Support\Enumerable;
+use JobVerplanke\LaravelActiveCampaign\Resources\Contact\CreateContact;
 
 class ExtendMapCreateContact extends MapCreateContact
 {
     public function map(Enumerable $data): array
     {
-        return array_merge(parent::map(data: $data), [
-            'foo' => 'bar'
-        ]);
+        return array_merge(
+            parent::map(data: $data),
+            ['foo' => 'bar']
+        );
     }
 }
+
+Route::get('create-contact-extend-map', function () {
+    $data = collect(value: [
+        'email' => 'johndoe@example.com',
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+    ]);
+
+    $createContact = new CreateContact();
+    $response = $createContact
+        ->mapUsing(class: CustomMapCreateContact::class)
+        ->execute(data: $data);
+
+    return $response->json();
+});
