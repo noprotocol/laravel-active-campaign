4f4e882fe7b199fa73005881e05da8d069cce634
Job Verplanke <job.verplanke@gmail.com>
2022-04-11 19:57:04 +0200

    Added delete class for delete endpoints

---
+++ src/Delete.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JobVerplanke\LaravelActiveCampaign;
+
+use JobVerplanke\LaravelActiveCampaign\Contracts\Mappable as MappableContract;
+use JobVerplanke\LaravelActiveCampaign\Traits\Mappable;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\Client\Response;
+
+/**
+ * @template TKey of int|string
+ * @template TValue
+ *
+ * @extends \JobVerplanke\LaravelActiveCampaign\ActiveCampaign<TKey, TValue>
+ * @implements MappableContract<TKey, TValue>
+ */
+abstract class Delete extends ActiveCampaign implements MappableContract
+{
+    use Mappable;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->client()->mapUsing();
+    }
+
+    /**
+     * @param array<int|string, mixed> $data
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    protected function delete(string $resource, array $data): Response
+    {
+        return $this->client->delete(url: $resource, data: $data)
+            ->throw(function (Response $response, RequestException $exception) {
+                report(exception: $exception);
+            });
+
+    }
+}
